d5bd5564430be24490633163f985dd16a8ed0698
Robert Cunningham <robertvcunningham@gmail.com>
2018-10-13 12:40:34 -0400

    Robert changes

--- sana.py
+++ sana.py
@@ -3,14 +3,18 @@ import time
 
 mydict = {"code":"import","jumpline":"12"}
 
+time.sleep(2)
+keyboard.press_and_release('ctrl+t')
+
 def execute(command,input):
+    time.sleep(2)
     if command == 'code':
         keyboard.write('i')
         keyboard.write(input)
         keyboard.press_and_release('esc')
-    elif (command == 'jumpline'):
+    elif command == 'jumpline':
         keyboard.write(':' + input + 'G') 
 
-for c in mydict:
-    execute(c,mydict[c])
-
+for c,v in mydict.items():
+    execute(c,v)
+    time.sleep(2)
