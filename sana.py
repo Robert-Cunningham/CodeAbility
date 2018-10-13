2ae2d4a1f794acfa17b3ed12ef9f04a9780a42db
Robert Cunningham <robertvcunningham@gmail.com>
2018-10-13 11:49:45 -0400

    Rename keyboard.py

--- sana.py
+++ sana.py
@@ -1,16 +1,16 @@
 import keyboard
 import time
 
-dict = {"code":"import","jumpline":"12"}
+mydict = {"code":"import","jumpline":"12"}
 
 def execute(command,input):
-    if (command == 'code'):
+    if command == 'code':
         keyboard.write('i')
         keyboard.write(input)
         keyboard.press_and_release('esc')
     elif (command == 'jumpline'):
         keyboard.write(':' + input + 'G') 
 
-for c in dict:
-    execute(c,dict[c])
+for c in mydict:
+    execute(c,mydict[c])
 
