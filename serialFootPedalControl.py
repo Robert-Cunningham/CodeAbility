bfc9e959904c4649430f19fb5682e17dc90d49e1
Ian Richardson <ian.irrtech@gmail.com>
2018-10-13 20:19:52 -0400

    Refactor Pedal Serial keying SHIFT NOT FUNCTIONAL

--- serialFootPedalControl.py
+++ serialFootPedalControl.py
@@ -2,62 +2,99 @@ import serial
 import keyboard
 import time
 
+numPedals = 5
+
 scroll_down = 'ctrl+alt+up'
 scroll_up = 'ctrl+alt+down'
 tab_left = 'ctrl+shift+tab'
 tab_right = 'ctrl+tab'
 
-doubleDelay = 700
-lastCodeTime = 0
-lastCode = -1
+#press actions (order in array is index of pedal)
+
+onKey = ["PTT", "SHIFT", scroll_down, scroll_up, tab_right]
+onKeySHIFT = ["PTT TOGGLE"] #FIX THIS
+              
+shiftState = False
 
 ser = serial.Serial("COM6", 9600)
 
 while True:
-    =int(ser.readline())
-    currentTime = int( round(time.time() * 1000) )
-    
-    print("EVENT! CODE:" + str(code)+ " LAST CODE: " + str(lastCode) + " CURRENT TIME: " + str(currentTime) + "lastCodeTime: "+ str(lastCodeTime))
-
+    code=int(ser.readline())
     
     #key presses
-    
-    if code == 0:
-        if lastCode == code and (currentTime - lastCodeTime) < doubleDelay :
-            print("double click!")
+
+    #~~~~~~~~~~~~~~~~~~~ #when shift is OFF# ~~~~~~~~~~~~~~~~~~~~~~#
+    if not shiftState:
+	    
+	######### press events ##########
+	    
+        if code < numPedals: 
+              
+            if onKey[code] == "PTT":
+                print("Code: " + str(code) +" PTT ON!")
+                #insert extra function here
+                  
+            elif onKey[code] == "SHIFT":
+                shiftState = True
+                print("Code: " + str(code) +" SHIFT ON!")
+                  
+            else:
+                keyboard.press(onKey[code])
+                print("Code: " + str(code) + " " + onKey[code])
+
+	######### release events ##########
+        
+        else: 
+            if onKey[code-numPedals] == "PTT":
+                print("Code: " + str(code) +" PTT OFF!")
+                #insert extra function here
+                  
+            elif onKey[code-numPedals] == "SHIFT":
+                shiftState = False
+                print("Code: " + str(code) +" SHIFT OFF (already was)!")
+                  
+            else:
+                keyboard.release(onKey[code-numPedals]) 
+                #keyboard.release(onKeySHIFT[code-numPedals]) #to make sure shifted keys are removed too #REMOVED UNTIL SHIFT FIXED!!!!!
+                print("Code: " + str(code) + " " + onKey[code-numPedals] +" OFF")
+                #print("(and) Code: shift of " + str(code) + " " + onKeySHIFT[code-numPedals] +" OFF")
+                
+            
+    #~~~~~~~~~~~~~~~~~~~ #when shift is ON# ~~~~~~~~~~~~~~~~~~~~~~#    
+    else: 
+
+	######### press events ##########
+        
+        if code < 5:
+              
+            if onKeySHIFT[code] == "PTT":
+                print("Code: " + str(code) +" PTT ON!")
+                #insert extra function here
+                  
+            elif onKeySHIFT[code] == "SHIFT":
+                shiftState = True
+                print("Code: " + str(code) +" SHIFT ON (already was)!")
+                  
+            else:
+                keyboard.press(onKeySHIFT[code])
+                print("Code: " + str(code) + " " + onKeySHIFT[code])
+
+	######### release events ##########
+        
         else:
-            keyboard.press(scroll_down)
-            print("single click")
-
-    elif code == 1:
-        keyboard.press(scroll_up)
-        
-        
-    elif code == 2:
-        keyboard.press(tab_left)
-        
-    elif code == 3:
-        keyboard.press(tab_right)
-        
-    #key releases
-
-    elif code == 4:
-        keyboard.release(scroll_down)
-        print("released")
-
-    elif code == 5:
-        keyboard.release(scroll_up)
-        
-    elif code == 6:
-        keyboard.release(tab_left)
-        
-    elif code == 7:
-        keyboard.release(tab_right)
-
-    if code in [0,1,2,3]:
-        lastCode = code
-    
-    lastCodeTime = currentTime
+            if onKey[code - numPedals] == "PTT":
+                print("Code: " + str(code) +" PTT OFF!")
+                #insert extra function here
+                  
+            elif onKey[code - numPedals] == "SHIFT":
+                shiftState = False
+                print("Code: " + str(code) + " SHIFT OFF!")
+                  
+            else:
+                keyboard.release(onKey[code - numPedals]) #maybe remove to PURPOSELY stick keys
+                keyboard.release(onKeySHIFT[code - numPedals]) #to make sure shifted keys are removed too
+                print("Code: " + str(code) + " " + onKeySHIFT[code - numPedals] +" OFF")
+                print("(and) Code: normal of" + " " + str(code) + onKey[code - numPedals] +" OFF")
         
         
         
